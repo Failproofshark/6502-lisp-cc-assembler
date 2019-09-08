0976224b59f07e70cc486c7a17b9284c9311cc6e
failproofshark <none@none.com>
2019-09-08 16:53:34 -0700

    Added absolute-x absolute-y and indirect addressing mode support

--- main.lisp
+++ main.lisp
@@ -63,12 +63,12 @@
 ;; todo In a two pass assembly process where we look for things like labels we may need an intermediary instead of directly writing the object code to the file
 ;; TODO need to check for syntax errors (e.g. unsupported address modes, malformed data etc)
 ;; TODO general process is similar to mnemonic opcode table generation: read opcode read argumements (anyhwere between 1 and 3 items, e.g. it's just an address but could also be address "," [x,y]. The most important bit to further process is the address. If it's 4 characters we need to split it up over two bytes in low byte high byte order
-;; TODO fix code being a list of lists in the end... can actually be kinda helpful for debugging maybe
 ;; TODO (note) adding expressions will change the way we translate addresses as we can have mathematical operators between parentheses
+;; TODO accept optional output stream argument
 (defun assemble (tokens)
   ;; TODO rename convert-address to something more appropriate
   ;; TODO add "is-indirect-p" argument to convert to initiate whether or not we are indirect addressing or not (all indirect address modes enclose their arguments in parens
-  (labels ((convert-address (address-string &key indrect-mode register)
+  (labels ((convert-address (address-string &key indirect-mode register)
              (cond ((char= (aref address-string 0) #\#)
                     `("-immediate" ,(parse-integer address-string :start 2 :radix 16)))
                    ((char= (aref address-string 0) #\a)
@@ -76,26 +76,39 @@
                    ((char= (aref address-string 0) #\$)
                     (if (= (length address-string) 3)
                         `("-zero-page" ,(parse-integer address-string :start 1 :radix 16))
-                        `("-absolute" (,(parse-integer address-string :start 3 :radix 16) ,(parse-integer address-string :start 1 :end 3 :radix 16)))))))
+                        `(,(cond (indirect-mode "-indirect")
+                                 ((equal "x" register) "-absolute-x")
+                                 ((equal "y" register) "-absolute-y")
+                                 (t "-absolute"))
+                          ,(parse-integer address-string :start 3 :radix 16) ,(parse-integer address-string :start 1 :end 3 :radix 16))))))
            (convert (tokens)
              ;; TODO handle branch opcodes separately
              (cond ((= 1 (length tokens))
                     `(,(gethash (car tokens) mnemonic-opcode-lookup)))
                    ((= 2 (length tokens))
-                    (let* ((converted-address-and-mode (convert-address (car tokens)))
-                           (opcode (cadr tokens))
+                    (let* ((converted-address-and-mode (convert-address (cadr tokens)))
+                           (opcode (car tokens))
+                           (completed-code (concatenate 'string opcode (car converted-address-and-mode))))
+                      (append 'nil `(,(gethash completed-code mnemonic-opcode-lookup)) (cdr converted-address-and-mode))))
+                   ((= 4 (length tokens))
+                    (let* ((converted-address-and-mode (if (equal "(" (cadr tokens))
+                                                           (convert-address (caddr tokens) :indirect-mode t)
+                                                           (convert-address (cadr tokens) :register (cadddr tokens))))
+                           (opcode (car tokens))
                            (completed-code (concatenate 'string opcode (car converted-address-and-mode))))
                       (break)
-                      `(,(gethash completed-code mnemonic-opcode-lookup) ,(cdr converted-address-and-mode)))))))
+                      (append 'nil `(,(gethash completed-code mnemonic-opcode-lookup)) (cdr converted-address-and-mode)))))))
     (let ((buffer 'nil)
           (object-code 'nil))
       (loop for token = (car tokens) while token do
-        (push token buffer)
+        (setf buffer (append buffer `(,token)))
         (if (or (gethash (cadr tokens) available-mnemonics)
                 (= (length (cdr tokens)) 0))
             (progn
-              (push (convert buffer) object-code)
+              (loop for byte in (convert buffer) do
+                    (push byte object-code))
               (setf buffer 'nil)))
         ;; We could use the standard loop-over-list syntax however we would need to add an extra call to convert anything remaining in the buffer after the loop ended. So instead we manually move along the list
         (setf tokens (cdr tokens)))
+      ;; we recorded the the buffer in reverse (pushing bytes) for speed purposes. We used append for the buffer read in because it doesn't grow as large as the final object code we wish to output
       object-code)))
